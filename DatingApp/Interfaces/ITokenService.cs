5eb0d4e272cd92fe80db6106a80a7241ca7e2c6c
susanta rout <rsusanta0@gmail.com>
2020-12-26 18:50:17 +0530

    Token Service Configured

---
+++ DatingApp/Interfaces/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿
+
+using DatingApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+    }
+}
