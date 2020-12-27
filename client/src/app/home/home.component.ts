e63e7781b79d695cf782982d142db0e2a97ae106
susanta rout <rsusanta0@gmail.com>
2020-12-27 17:24:16 +0530

    parent to child communication

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   registerMode=false;
+  user:any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
   registerToggle()
   {
     this.registerMode=!this.registerMode;
+  }
+  getUsers()
+  {
+    this.http.get('http://localhost:51111/api/users').subscribe(users => this.user=users)
   }
 
 }
