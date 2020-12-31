e8839415677d51410832766ca5c512d7482f2d31
susanta rout <rsusanta0@gmail.com>
2020-12-31 13:42:33 +0530

    error controller for testing error

--- DatingApp/DTOs/RegisterDto.cs
+++ DatingApp/DTOs/RegisterDto.cs
@@ -11,6 +11,9 @@ namespace DatingApp.DTOs
        [Required]
        public string UserName { get; set; }
        [Required]
+       [StringLength(8,MinimumLength =4)]
        public string Password { get; set; }
+
+      
     }
 }
