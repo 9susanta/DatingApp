8c7812b0e8aa78069a11ce0a86fad8ece5c306c7
susanta rout <rsusanta0@gmail.com>
2021-01-06 23:23:23 +0530

    Setting Up Main Photo in API

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -7,14 +7,11 @@ import { MemberEditComponent } from '../members/member-edit/member-edit.componen
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(
-    component: MemberEditComponent,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (component.editForm.dirty) {
-        return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
-      }
+  canDeactivate(component: MemberEditComponent): boolean {
+    if (component.editForm.dirty) {
+      return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
+    }
+    return true;
   }
   
 }
--- client/src/app/_interceptor/loading.interceptor.ts
+++ client/src/app/_interceptor/loading.interceptor.ts
@@ -19,7 +19,6 @@ export class LoadingInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       delay(1000),
       finalize(() => {
-        debugger;
         this.busyService.idle();
       })
     );
