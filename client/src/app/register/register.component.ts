4bde98bee6a62112b1140a5fe04c594e763f08b2
susanta rout <rsusanta0@gmail.com>
2021-01-08 21:37:48 +0530

    Reactive From Added

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -12,20 +13,31 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister=new EventEmitter();
 
   model:any={};
+  registerForm: FormGroup;
 
   constructor(private accountService:AccountService,private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.intitializeForm();
   }
-
+  intitializeForm()
+  {
+    this.registerForm=new FormGroup({
+      username:new FormControl(),
+      password:new FormControl(),
+      confirmPassword:new FormControl()
+    });
+  }
   register()
   {
-    this.accountService.register(this.model).subscribe(response=>{
-      console.log(response);
-      this.cancel();
-    },error=>{
-      this.toastr.error(error.error);
-    })
+    console.log(this.registerForm);
+
+    // this.accountService.register(this.model).subscribe(response=>{
+    //   console.log(response);
+    //   this.cancel();
+    // },error=>{
+    //   this.toastr.error(error.error);
+    // })
   }
   cancel()
   {
