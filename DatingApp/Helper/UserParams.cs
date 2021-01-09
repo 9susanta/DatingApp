731e60ee894f9f39f0b5b8b4d2d53be380996115
susanta rout <rsusanta0@gmail.com>
2021-01-09 23:46:46 +0530

    Additional Filter Added

--- DatingApp/Helper/UserParams.cs
+++ DatingApp/Helper/UserParams.cs
@@ -18,8 +18,8 @@ namespace DatingApp.Helper
         }
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
-        //public int MinAge { get; set; } = 18;
-        //public int MaxAge { get; set; } = 150;
-        //public string OrderBy { get; set; } = "lastActive";
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 150;
+        public string OrderBy { get; set; } = "lastActive";
     }
 }
