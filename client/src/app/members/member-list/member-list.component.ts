3d2dea0b7d3981b5773a0c15e8821c6075aa4b8b
susanta rout <rsusanta0@gmail.com>
2021-01-10 18:21:07 +0530

    client side Fiter for a user in service

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -20,17 +20,15 @@ export class MemberListComponent implements OnInit {
   user: User;
   genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
 
-  constructor(private memberService: MembersService,private accountService: AccountService) { 
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
-      this.user = user;
-      this.userParams = new UserParams(user);
-    })
+  constructor(private memberService: MembersService) { 
+    this.userParams = this.memberService.getUserParams();
   }
 
   ngOnInit(): void {
     this.loadMembers();
   }
   loadMembers() {
+    this.memberService.setUserParams(this.userParams);
     this.memberService.getMembers(this.userParams).subscribe(response => {
       this.members = response.result;
       this.pagination = response.pagination;
@@ -43,6 +41,7 @@ export class MemberListComponent implements OnInit {
   pageChanged(event: any)
   {
     this.userParams.pageNumber=event.page;
+    this.memberService.setUserParams(this.userParams);
     this.loadMembers();
   }
 }
