19cf17dda2ea56202dd75bc49e51daf7dfff4e3a
susanta rout <rsusanta0@gmail.com>
2021-01-14 23:31:32 +0530

    Resolved Tab Issue

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -24,7 +24,10 @@ export class MemberDetailComponent implements OnInit {
     private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.loadMember();
+
+    this.route.data.subscribe(data => {
+      this.member = data.member;
+    })
 
     this.route.queryParams.subscribe(params => {
       params.tab ? this.selectTab(params.tab) : this.selectTab(0);
@@ -40,6 +43,7 @@ export class MemberDetailComponent implements OnInit {
         preview: false
       }
     ]
+    this.galleryImages=this.getImages();
   }
   
   getImages(): NgxGalleryImage[] {
@@ -54,12 +58,11 @@ export class MemberDetailComponent implements OnInit {
     return imageUrls;
   }
 
-  loadMember() {
-    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
-      this.member = member;
-      this.galleryImages=this.getImages();
-    })
-  }
+  // loadMember() {
+  //   this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
+  //     this.member = member;
+  //   })
+  // }
 
   loadMessages() {
     this.messageService.getMessageThread(this.member.username).subscribe(messages => {
