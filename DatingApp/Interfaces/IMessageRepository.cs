cfc98e6f7bdf999a33017f617f19774be9202f1e
susanta rout <rsusanta0@gmail.com>
2021-01-14 14:41:01 +0530

    setting up the message repository

---
+++ DatingApp/Interfaces/IMessageRepository.cs
@@ -0,0 +1,20 @@
+﻿using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserId, string recipientUserId);
+        Task<bool> SaveAllAsync();
+    }
+}
