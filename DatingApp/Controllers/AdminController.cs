1ce8bbd209480d228b4201a24ccef15a4137dcaa
susanta rout <rsusanta0@gmail.com>
2021-01-17 19:51:20 +0530

    Added PolicyBased Authorization

---
+++ DatingApp/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using DatingApp.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+        //private readonly UserManager<AppUser> _userManager;
+        //public AdminController(UserManager<AppUser> userManager)
+        //{
+        //    _userManager = userManager;
+        //}
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("users-with-roles")]
+        public async Task<ActionResult> GetUsersWithRoles()
+        {
+            //var users = await _userManager.Users
+            //    .Include(r => r.UserRoles)
+            //    .ThenInclude(r => r.Role)
+            //    .OrderBy(u => u.UserName)
+            //    .Select(u => new
+            //    {
+            //        u.Id,
+            //        Username = u.UserName,
+            //        Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+            //    })
+            //    .ToListAsync();
+
+            return Ok("Only Admin can access this");
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photos-to-moderate")]
+        public ActionResult GetPhotosForModeration()
+        {
+            return Ok("Admins or moderators can see this");
+        }
+    }
+}
