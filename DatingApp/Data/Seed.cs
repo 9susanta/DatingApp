d9df2595d5af515393b9a91908393ad677249ee3
susanta rout <rsusanta0@gmail.com>
2021-01-17 19:07:39 +0530

    Adding Role to Application

--- DatingApp/Data/Seed.cs
+++ DatingApp/Data/Seed.cs
@@ -9,13 +9,23 @@ namespace DatingApp.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             if (users == null) return;
+            var roles = new List<AppRole>
+            {
+                new AppRole { Name = "Member" },
+                new AppRole { Name = "Admin" },
+                new AppRole { Name = "Moderator" },
+            };
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
             foreach (var user in users)
             {
 
@@ -23,7 +33,15 @@ namespace DatingApp.Data
 
 
                 await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user,"Member");
             }
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" });
         }
     }
 }
