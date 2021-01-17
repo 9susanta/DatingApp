055488b29ba6d516a2ec6e998456cfccbdb8d3ae
susanta rout <rsusanta0@gmail.com>
2021-01-17 14:23:14 +0530

    dotnet ef database drop
    updating seed

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -1,5 +1,7 @@
 using DatingApp.Data;
+using DatingApp.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,8 +25,9 @@ namespace DatingApp
             try
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                //await Seed.SeedUsers(context);
+                await Seed.SeedUsers(userManager);
             }
             catch (Exception ex)
             {
