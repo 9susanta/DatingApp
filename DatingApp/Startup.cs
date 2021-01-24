02a432c515cdcc9a16c8756f1e9f7eb30af7bdcb
susanta rout <rsusanta0@gmail.com>
2021-01-24 17:02:37 +0530

    added fallback controller

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -73,6 +73,7 @@ namespace DatingApp
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
